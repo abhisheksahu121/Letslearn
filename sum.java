5fa5adf36f215675de4ce6a06270d64bc21666d2
Abhishek sahu <Abhishek4032sahu@gmail.com>
2021-06-01 12:54:21 +0530

    enter text this is sum

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum{
     public static void main(String args[]){
         int a = 3;
         int b = 6;
-        System.out.println(a+b);
+        System.out.println("The sum is" a+b);
     }
 
 }
